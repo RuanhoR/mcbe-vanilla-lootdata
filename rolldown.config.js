2b9ed406c94104414af236d160cbfda03f24ce54
ruanhor <3915264929@qq.com>
2026-08-16 16:34:55 +0800

    Initial Commit

---
+++ rolldown.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+import { dts } from "rolldown-plugin-dts";
+/**
+ * @type {import("rolldown").RolldownOptions[]}
+ */
+export default [
+  {
+    plugins: [dts()],
+    input: "./src/index.ts",
+    platform: "node",
+    output: [
+      {
+        entryFileNames: "[name].js",
+        minify: true,
+        dir: "./dist",
+        cleanDir: true,
+        format: "esm",
+        postBanner:
+          "/*License: MIT, https://github.com/RuanhoR/mcbe-vanilla-lootdata*/",
+      },
+    ],
+  },
+];
